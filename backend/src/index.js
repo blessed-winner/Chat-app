f8624ec2b359287e058f676e14f5992ad105b9fc
Winner <blessedwinner66@gmail.com>
2025-10-16 21:44:58 +0200

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js'
+
+dotenv.config()
+
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+
+app.get('/api/auth',authRoutes)
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`)
+    connectDB()
+})
