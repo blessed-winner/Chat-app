622bac065b74f87f165dc8ee719eb06af9d5fb46
Winner <blessedwinner66@gmail.com>
2025-10-17 13:43:44 +0200

    Added cors and resolved axios errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/message.route.js'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -11,6 +12,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
