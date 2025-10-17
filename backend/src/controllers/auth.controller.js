4d328b2dd1add1d8b9dba2075f5435cc7fc19464
Winner <blessedwinner66@gmail.com>
2025-10-17 09:18:42 +0200

    Cloudinary configuration added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js"
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
@@ -74,6 +75,23 @@ export const logout = (req,res) => {
     }
 }
 
-export const updateProfile = (req,res) => {
-  
+export const updateProfile = async (req,res) => {
+    const { profilePic } = req.body
+    const userId = req.user._id
+     try {
+        if(!profilePic) return res.status(400).json({ message:"Profile picture is required" })
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await this.User.findByIdAndUpdate( 
+          userId, 
+          { profilePic:uploadResponse.secure_url }, 
+          { new:true } 
+        )
+
+        return res.status(200).json(updatedUser)
+     } catch (error) {
+        console.log("Error in updateProfile controller",error.message)
+        return res.status(500).json({ message:"Internal server error" })
+     }
 }
+
+
