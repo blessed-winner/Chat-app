6861a3bba581c1a694417953e8a535a1e1cd8bc2
Winner <blessedwinner66@gmail.com>
2025-10-21 19:38:56 +0200

    Added various daisy-ui themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,11 @@ import { useAuthStore } from './store/useAuthStore.js'
 import { useEffect } from 'react'
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore.js'
 
 const App = () => {
   const { authUser,checkAuth, isCheckingAuth } = useAuthStore()
+  const{theme} = useThemeStore()
 
   useEffect(()=>{
     checkAuth()
@@ -29,7 +31,7 @@ const App = () => {
         }
        
   return (
-    <div>
+    <div data-theme={theme}>
        <Navbar/>
         <Routes>
            <Route path='/' element={authUser ? <HomePage/> : <LoginPage/>}/>
