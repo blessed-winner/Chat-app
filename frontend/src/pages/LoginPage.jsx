dd744e172868d26ec83004fd51097d8db273669c
Winner <blessedwinner66@gmail.com>
2025-10-21 17:07:28 +0200

    Implementing themes setting

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useAuthStore } from '../store/useAuthStore'
+import { useAuthStore } from '../store/useAuthStore.js'
 import { Eye, EyeOff, Loader2, Mail, MessageSquare,Lock } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import AuthImagePattern from '../components/AuthImagePattern'
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAuthStore } from '../store/useAuthStore'
+import { useAuthStore } from '../store/useAuthStore.js'
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
