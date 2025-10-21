0b3b53a919e6b78eac92dd6c3d7e5b9f7d24df90
Winner <blessedwinner66@gmail.com>
2025-10-21 21:01:14 +0200

    Implementing home page features

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,34 @@
+import {create} from 'zustand'
+import toast from 'react-hot-toast'
+import { axiosInstance } from '../lib/axios'
+
+export const useChatStore = ((set)=>({
+    users:[],
+    messages:[],
+    selectedUser:null,
+    isUsersLoading:false,
+    isMessagesLoading:false,
+    getUsers: async () => {
+        set({isUsersLoading:true})
+        try {
+         const res = await axiosInstance.get('/message/users')
+         set({users:res.data})
+        } catch (error) {
+          toast.error(error?.response?.data?.message || 'Problem getting users')  
+        }finally{
+           set({isUsersLoading:false}) 
+        }
+    },
+
+    getMessages: async (userId) => {
+     set({isMessagesLoading:true})
+     try {
+      const res = await axiosInstance.get(`/message/${userId}`)
+      set({messages:res.data})  
+     } catch (error) {
+        toast.error(error?.response?.data?.message || 'Problem getting messages')
+     } finally {
+        set({isMessagesLoading:false})
+     }
+    }
+}))
