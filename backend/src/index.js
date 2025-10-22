3bc759d9b6d4e6d640b2c04728a4a443bf2f1bfc
Winner <blessedwinner66@gmail.com>
2025-10-22 11:15:14 +0200

    Configured socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,10 @@ import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/message.route.js'
 import cors from 'cors'
+import { app,server,io } from './lib/socket.js'
 
 dotenv.config()
 
-
-const app = express()
 const port = process.env.PORT || 5001
 
 app.use(cors({
@@ -23,7 +22,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is running on port: ${port}`)
     connectDB()
 })
