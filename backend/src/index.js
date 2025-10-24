f9771eeacfa24aba1c858e180eafb0d27ad59421
Winner <blessedwinner66@gmail.com>
2025-10-24 09:49:59 +0200

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/message.route.js'
 import cors from 'cors'
 import { app,server,io } from './lib/socket.js'
+import path from 'path'
 
 dotenv.config()
 
 const port = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 app.use(cors({
     origin:'http://localhost:5173',
@@ -21,6 +23,13 @@ app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 server.listen(port,()=>{
     console.log(`Server is running on port: ${port}`)
